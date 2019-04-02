5ae7c464a7c99665c91ed335bebdcdb982446204
sugitahisaya <sugitahisaya@team-lab.com>
2019-04-02 17:27:47 +0900

    Service.swiftの書き方を少し変更

--- RealtimeQuestionnaire/Extension/Service.swift
+++ RealtimeQuestionnaire/Extension/Service.swift
@@ -9,51 +9,39 @@
 import Foundation
 import KeychainAccess
 
-let S: Service.Type = Service.self
-
-let SERVICE_NAME = "com.hisayasugita.RealtimeQuestionnaire"
-
-struct Service {
+final class ServicesUtil {
     private init() {}
-    static let keychain = Keychain(service: SERVICE_NAME)
+    static let shared = ServicesUtil()
+    static let keychain = Keychain(service: "com.hisayasugita.RealtimeQuestionnaire")
     static let userDefaults = UserDefaults.standard
 }
 
 // MARK: Keychain
-enum Keychain_Keys: String {
-    case uid
-}
-
-extension Service {
-    static func getKeychain(_ key: Keychain_Keys) -> String? {
+extension ServicesUtil {
+    static func getKeychain(_ key: KeychainKeys) -> String? {
         return keychain[key.rawValue]
     }
     
-    static func setKeychain(_ key: Keychain_Keys, _ value: String?) {
+    static func setKeychain(_ key: KeychainKeys, _ value: String?) {
         keychain[key.rawValue] = value
     }
 }
 
 // MARK: UserDefaults
-enum UserDefaults_Keys: String {
-    // Sample
-    case isFirstLaunch
-}
-
-extension Service {
+extension ServicesUtil {
     static func setDefaultUserDefaults() {
-        userDefaults.register(defaults: [UserDefaults_Keys.isFirstLaunch.rawValue: true])
+        userDefaults.register(defaults: [UserDefaultsKeys.isLogin.rawValue: true])
     }
     
-    static func setUserDefaults<T>(_ key: UserDefaults_Keys, _ value: T) {
+    static func setUserDefaults<T>(_ key: UserDefaultsKeys, _ value: T) {
         userDefaults[key] = value
     }
     
-    static func getUserDefaults<T: Any>(_ key: UserDefaults_Keys) -> T? {
+    static func getUserDefaults<T: Any>(_ key: UserDefaultsKeys) -> T? {
         return userDefaults[key]
     }
     
-    static func removeUserDefaults(_ key: UserDefaults_Keys) {
+    static func removeUserDefaults(_ key: UserDefaultsKeys) {
         userDefaults.remove(key)
     }
 }
